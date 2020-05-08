b10b7e9dad5cf7d8089be1c096373bfd5fe27b6c
grek97 <omarnabiev20@yandex.ru>
2020-05-08 18:39:48 +0300

    add editions

--- Rock_Scissors_Paper.rb
+++ Rock_Scissors_Paper.rb
@@ -12,41 +12,67 @@ gek_box  = []
 loop do 
   arr = %w[Rock Scissors Paper]
   
+  puts "\t\t\tИГРА"
   puts
-  puts  "Варианты для выбора: 1 - Rock 2 - Scissors 3 - Paper"
-  print "Chuk выбор за вами: "
+  puts  'Варианты для выбора: 1 - Rock 2 - Scissors 3 - Paper'
+  print 'Chuk выбор за вами: '
   chuk_choice = get
   clear
   
-  puts  "Варианты для выбора: 1 - Rock 2 - Scissors 3 - Paper"
-  print "Gek выбор за вами: "
+  puts  'Варианты для выбора: 1 - Rock 2 - Scissors 3 - Paper'
+  print 'Gek выбор за вами: '
   gek_choice = get
   clear
 
-  puts "РЕЗУЛЬТАТЫ:"
   puts "Chuk выбрал:\t #{arr[chuk_choice]}"
   puts "Gek выбрал:\t #{arr[gek_choice]}"
   puts
-
+  
+  chuk_choice = nil
+  gek_choice  = nil
+  loop do
+    if chuk_choice > 3 && chuk_choice < 0
+      puts 'Выберите 1 2 3'
+      chuk_choice
+    
+    elsif gek_choice > 3 && gek_choice < 0
+      puts 'Выберите 1 2 3'
+      gek_choice
+    else 
+      break
+  end
   if chuk_choice == gek_choice
     puts "\t\t\tНИЧЬЯ"
 
   elsif chuk_choice == 0 && gek_choice == 1
-    puts "\t\t\tВыиграл Chuk"
+    puts "\t\t Выиграл Chuk"
     chuk_box<< chuk_choice
   
   elsif chuk_choice == 1 && gek_choice == 2 
-    puts "\t\t\tВыиграл Chuk"
+    puts "\t\t Выиграл Chuk"
     chuk_box<< chuk_choice
   
   elsif chuk_choice == 2 && gek_choice == 0
-    puts "\t\t\tВыиграл Chuk"
+    puts "\t\t Выиграл Chuk"
     chuk_box<< chuk_choice
   
-  else puts "\t\t\tВыиграл Gek"
+  else puts "\t\t Выиграл Gek"
     gek_box<< gek_choice
   end 
- 
 
+  puts 'СЧЕТ:'
+  puts "Chuk\t #{chuk_box.size}"
+  puts "Gek\t #{gek_box.size}"
+  
+  if chuk_box.size > gek_box.size
+    puts 'Выигрывает Chuk'
+  elsif chuk_box.size < gek_box.size
+    puts 'Выигрывает Gek'
+  else puts 'Счет равен'
+  end
+  puts '===================================================='
+  sleep 2 
+  clear
 end
+
 
