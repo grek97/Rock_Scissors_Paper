013aea0161c3485ecbca868d0562f7f8a2a3cb09
grek97 <omarnabiev20@yandex.ru>
2020-05-08 17:54:33 +0300

    Task next

--- Rock_Scissors_Paper.rb
+++ Rock_Scissors_Paper.rb
@@ -6,7 +6,27 @@ def get
 end    
 clear
    
+chuk_box = []
+gek_box  = []
+
 loop do 
+  arr = %w[Rock Scissors Paper]
+  
+  puts
+  puts  "Варианты для выбора: 1 - Rock 2 - Scissors 3 - Paper"
+  print "Chuk выбор за вами: "
+  chuk_choice = get
+  clear
+  
+  puts  "Варианты для выбора: 1 - Rock 2 - Scissors 3 - Paper"
+  print "Gek выбор за вами: "
+  gek_choice = get
+  clear
+
+  puts "РЕЗУЛЬТАТЫ:"
+  puts "Chuk выбрал:\t #{arr[chuk_choice]}"
+  puts "Gek выбрал:\t #{arr[gek_choice]}"
+  puts
 
   if chuk_choice == gek_choice
     puts "\t\t\tНИЧЬЯ"
